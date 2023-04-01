8f0ca948b8e9566e4d92e96e250f1ebc21147f58
robolev <92596597+robolev@users.noreply.github.com>
2023-04-01 21:01:14 +0200

    Changed folder

---
+++ Maze/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Maze
+{
+    internal class Program
+    {
+        Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+
+        static char[,] world;
+
+        int playerX;
+        int playerY;
+
+        static void Main(string[] args)
+        {
+            GenerateWorld();
+
+
+        }
+
+        static void GenerateWorld()
+        {
+            Random rnd = new Random();
+
+            world = new char[Console.WindowHeight, Console.WindowWidth];
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                for (int j = 0; j < Console.WindowHeight; j++)
+                {
+                    char symbol = '.';
+
+                    world[i, j] = symbol;
+                }
+            }
+        }
+
+        static void Input()
+        {
+
+
+        }
+        static void DrawPLayerPosition(int x, int y)
+        {
+
+
+
+        }
+
+        static void GameLoop()
+        {
+
+
+
+        }
+    }
+}
